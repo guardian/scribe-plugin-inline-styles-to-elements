896970e3057fdc4d9c12629a2dab8fb8ec28d516
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-05-01 12:22:43 +0100

    Fix language

--- test/specs/unit.spec.js
+++ test/specs/unit.spec.js
@@ -1,5 +1,5 @@
 define(['src/formatter'], function (formatter) {
-  it('should return wrap the content of a bold paragraph in a B element', function() {
+  it('should wrap the content of a bold paragraph in a B element', function() {
     var root = document.createElement('div');
     root.innerHTML = '<p style="font-weight: bold">1</p>';
 
@@ -7,7 +7,7 @@ define(['src/formatter'], function (formatter) {
     expect(output).to.equal('<p style=""><b>1</b></p>');
   });
 
-  it('should return wrap the content of a bold SPAN in a B element', function() {
+  it('should wrap the content of a bold SPAN in a B element', function() {
     var root = document.createElement('div');
     root.innerHTML = '<p>1 <span style="font-weight: bold">2</span></p>';
 
