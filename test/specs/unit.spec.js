c23f96c4f31d4cedb63e215ad9ccc809881a111f
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-05-02 10:58:21 +0100

    Test all mapped styles

--- test/specs/unit.spec.js
+++ test/specs/unit.spec.js
@@ -1,17 +1,52 @@
 define(['src/inline-styles-formatter'], function (formatter) {
-  it('should wrap the content of a bold paragraph in a B element', function() {
-    var root = document.createElement('div');
-    root.innerHTML = '<p style="font-weight: bold">1</p>';
 
-    var output = formatter(root).innerHTML;
-    expect(output).to.equal('<p style=""><b>1</b></p>');
+  function createContainer(html) {
+    var container = document.createElement('div');
+    container.innerHTML = html;
+    return container;
+  }
+
+
+  // Style to element mappings to test
+
+  var mappings = [{
+    name: 'bold',
+    element: 'b',
+    style: 'font-weight: bold'
+  },{
+    name: 'italic',
+    element: 'i',
+    style: 'font-style: italic'
+  },{
+    name: 'strike-through',
+    element: 'strike',
+    style: 'text-decoration: line-through'
+  },{
+    name: 'underline',
+    element: 'u',
+    style: 'text-decoration: underline'
+  }];
+
+
+  describe('inline-styles-formatter', function() {
+
+    mappings.forEach(function(mapping) {
+      it('should wrap the content of a '+mapping.name+' paragraph in a '+mapping.element.toUpperCase()+' element', function() {
+        var root = createContainer('<p style="' +mapping.style+ '">1</p>');
+        var output = formatter(root).innerHTML;
+        expect(output).to.equal('<p style=""><' +mapping.element+ '>1</' +mapping.element+ '></p>');
+      });
+
+      it('should wrap the content of a '+mapping.name+' SPAN in a ' +mapping.element.toUpperCase()+ ' element', function() {
+        var root = createContainer('<p>1 <span style="'+mapping.style+'">2</span></p>');
+        var output = formatter(root).innerHTML;
+        expect(output).to.equal('<p>1 <span style=""><' +mapping.element+ '>2</' +mapping.element+ '></span></p>');
+      });
+    });
+
   });
 
-  it('should wrap the content of a bold SPAN in a B element', function() {
-    var root = document.createElement('div');
-    root.innerHTML = '<p>1 <span style="font-weight: bold">2</span></p>';
 
-    var output = formatter(root).innerHTML;
-    expect(output).to.equal('<p>1 <span style=""><b>2</b></span></p>');
-  });
+  // TODO: test end to end via Scribe plugin registration
+
 });
